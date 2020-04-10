e4c87ff26ab2ccaa94d3a332a04601dbe3e91650
Timotije Sabo <57005164+timko7@users.noreply.github.com>
2020-04-10 18:51:42 +0200

    Implementirano povlacenje certifikata i vracanje povucenih.

--- APM/src/app/listaSertifikata/listaSertifikata.component.ts
+++ APM/src/app/listaSertifikata/listaSertifikata.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 import { ActivatedRoute, Router } from '@angular/router';
 import { RegistracijatServces } from '../registacija/registracija.sevice';
@@ -11,49 +11,73 @@ import { Sertifikat } from '../sertifikat/Sertifikat';
 @Component({
 
     //selector: 'login-component',
-    templateUrl : './listaSertifikata.html',
+    templateUrl: './listaSertifikata.html',
     //styleUrls: []
 
 })
 
 
-export class ListaSertifikataComponent implements OnInit{
-    
-  
-    listaCASertifikata:Sertifikat[]=[];
-    listaEESertifikata:Sertifikat[]=[];
+export class ListaSertifikataComponent implements OnInit {
 
-    constructor(private router:Router,private loginService:RegistracijatServces,private sertifikatService:SertifikatServces){
 
-  
-      
+    listaCASertifikata: Sertifikat[] = [];
+    listaEESertifikata: Sertifikat[] = [];
+    listaPovucenihSertifikata: Sertifikat[] = [];
+
+    constructor(private router: Router, private loginService: RegistracijatServces, private sertifikatService: SertifikatServces) {
+
+
+
     }
 
     ngOnInit() {
-     this.sertifikatService.vratiSveCA().subscribe({
-        next:listaCASertifikata=>{this.listaCASertifikata=listaCASertifikata;
-        console.log(this.listaCASertifikata);
-    }})
-    this.sertifikatService.vratiSveEE().subscribe({
-        next:listaEESertifikata=>{this.listaEESertifikata=listaEESertifikata;
-        console.log(this.listaEESertifikata);
-    }})
-  
-}
-povuciSertifikat(){
- 
- 
-    
-}
+        this.sertifikatService.vratiSveCA().subscribe({
+            next: listaCASertifikata => {
+                this.listaCASertifikata = listaCASertifikata;
+                console.log(this.listaCASertifikata);
+            }
+        })
+        this.sertifikatService.vratiSveEE().subscribe({
+            next: listaEESertifikata => {
+                this.listaEESertifikata = listaEESertifikata;
+                console.log(this.listaEESertifikata);
+            }
+        })
+        this.sertifikatService.vratiSvePovucene().subscribe({
+            next: listaPovucenihSertifikata => {
+                this.listaPovucenihSertifikata = listaPovucenihSertifikata;
+            }
+        })
 
-skiniSertifikatCA(uid:string){
-    console.log(uid);
-    this.sertifikatService.skiniCA(uid).subscribe();
+    }
 
-}
-skiniSertifikatEE(uid:string){
-    console.log(uid);
-    this.sertifikatService.skiniEE(uid).subscribe();
+    public povuciSertifikatCA(uid: string) {
+        this.sertifikatService.povuciCA(uid).subscribe(
+            data => {
+                console.log('Prosao povuci sertCA!!');
+                window.location.reload();
+            },
+            error => alert('Neuspesno povlacenje sertifikataCA!'));
+    }
 
-}
+    public povuciSertifikatEE(uid: string) {
+        this.sertifikatService.povuciEE(uid).subscribe(
+            data => {
+                console.log('Prosao povuci sertEE!!');
+                window.location.reload();
+            },
+            error => alert('Neuspesno povlacenje sertifikataEE!')
+        );
+    }
+
+    skiniSertifikatCA(uid: string) {
+        console.log(uid);
+        this.sertifikatService.skiniCA(uid).subscribe();
+
+    }
+    skiniSertifikatEE(uid: string) {
+        console.log(uid);
+        this.sertifikatService.skiniEE(uid).subscribe();
+
+    }
 }
